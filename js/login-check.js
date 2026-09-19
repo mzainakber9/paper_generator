f59c547b386d13908ab3419b44f42bbd31181d2c
mzainakber9 <104532455+mzainakber9@users.noreply.github.com>
2026-09-19 17:43:05 +0500

    Update ACCOUNTS object with new username

--- js/login-check.js
+++ js/login-check.js
@@ -7,7 +7,7 @@
   here you wouldn't be okay with a curious student seeing.
 */
 const ACCOUNTS = {
-  "zain": "123"
+  "akber": "123"
 };
 
 /* Checks a username/password pair against ACCOUNTS above. */
