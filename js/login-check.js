48ec9c1bdf2eea7cdef18f932e1044a78ff4d75c
mzainakber9 <104532455+mzainakber9@users.noreply.github.com>
2026-09-23 09:18:08 +0500

    Update password for user 'zain' in login-check.js

--- js/login-check.js
+++ js/login-check.js
@@ -7,7 +7,7 @@
   here you wouldn't be okay with a curious student seeing.
 */
 const ACCOUNTS = {
-  "zain": "changeme123"
+  "zain": "5678"
 };
 
 /* Checks a username/password pair against ACCOUNTS above. */
